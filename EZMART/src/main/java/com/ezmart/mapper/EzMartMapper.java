ee63a8258be57deaed4fd5370b6860b868b99da6
SMHRD <0611cjy@gmail.com>
2022-08-02 16:20:58 +0900

    지연 - 백

--- EZMART/src/main/java/com/ezmart/mapper/EzMartMapper.java
+++ EZMART/src/main/java/com/ezmart/mapper/EzMartMapper.java
@@ -1,5 +1,11 @@
 package com.ezmart.mapper;
 
+import com.ezmart.domain.MemberVO;
+
 public interface EzMartMapper {
+	
+	public int memberJoin(MemberVO vo);
+	
+	public MemberVO memberLogin(MemberVO vo);
 
 }
--- EZMART/src/main/java/com/ezmart/web/EzMartController.java
+++ EZMART/src/main/java/com/ezmart/web/EzMartController.java
@@ -1,10 +1,17 @@
 package com.ezmart.web;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.ezmart.domain.MemberVO;
 import com.ezmart.mapper.EzMartMapper;
+import org.springframework.ui.Model;
 
 @Controller
 public class EzMartController {
@@ -13,4 +20,35 @@ public class EzMartController {
 	
 	@RequestMapping("/main.do")
 	public void main() {}
+	
+	// 1. 회원가입
+	@RequestMapping("/Join.do")
+	public @ResponseBody String memerJoin(MemberVO vo) {
+		
+		System.out.println(vo);
+		
+		mapper.memberJoin(vo);
+		return "redirect:/main.do";
+		
+	}
+	
+	   @RequestMapping("/Login.do")
+	   public String gologin() {
+	      return "login";
+	   }
+	   
+	   // 2. 로그인
+	    @PostMapping("/Login.do")
+	    public String login(MemberVO vo, Model model) {
+	        MemberVO result = mapper.memberLogin(vo);
+	        String url = "";
+	        model.addAttribute("vo", vo);
+	        if (result != null) {
+	          url = "redirect:/main.do";
+	       } else {
+	          url = "redirect:/login.do";
+	       }
+	       
+	       return url;
+	    }
 }
